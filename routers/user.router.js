40fa90ec46d4b0499ec419e13edce05c35c49bd5
Hilcbek <balemayehu07@gmail.com>
2023-11-13 01:27:31 +0300

    adsadsd

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Login, Logout, Register } from "../controllers/user.controller.js";
+import { Login, Register } from "../controllers/user.controller.js";
 export let userRouter = express.Router();
 userRouter.post("/register", Register);
 userRouter.post("/login", Login);
